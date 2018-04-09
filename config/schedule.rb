6b92062933a12f0a2710e4b38f0191d907aa4c04
Fortunate-MAN <shivashish.ahuja@gmail.com>
2018-04-09 10:58:40 +0530

    Small changes

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,5 +2,5 @@
 # See more: https://github.com/javan/whenever
 
 every 15.minutes do
-  runner 'PostLog.get_statuses', environment: "development", mailto: 'shivashish.ahuja@gmail.com'
+  runner 'PostLog.get_statuses', environment: "development"
 end
