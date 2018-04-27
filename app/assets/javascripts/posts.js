3470ea182fe78281c17436645866f28674a03f22
Fortunate-MAN <shivashish.ahuja@gmail.com>
2018-04-27 14:53:34 +0530

    Remove not working code to make dialog draggable

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -51,7 +51,6 @@ $(document).ready(function() {
         .done(function(data) {
             var items = data['items'];
             var dialog = $(".modal-body").first();
-            dialog.draggable();
             var closeVoteItems = null             
 
             for (var i = 0; i < items.length; i ++) {
