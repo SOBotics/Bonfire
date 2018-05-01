bbb35a19968a3fcdead0ebd02647def638ab25a4
Fortunate-MAN <shivashish.ahuja@gmail.com>
2018-05-01 06:41:55 +0200

    Change devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,9 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'bonfire@bonfire.sobotics.org'
+  config.secret_key = '25a4a8973218974fde3c30c182b867bf9cc61dc2ded1d9eeff04a64ac7ca64228f53056c787c7f3f983567be2151a8e9f34590a6efaa7edd864e5713bd5b08a8'
+
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
